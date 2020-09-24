6f1afcd606f77b7f0fabe50c5ebf05267ac3c986
github username <github email address>
2020-09-24 11:35:18 -0400

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :appearances
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests #, only: [:index]
-  resources :episodes #, only: [:index]
+  resources :appearances, only: [:new, :create]
+  resources :guests, only: [:index, :show]
+  resources :episodes, only: [:index, :show]
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
