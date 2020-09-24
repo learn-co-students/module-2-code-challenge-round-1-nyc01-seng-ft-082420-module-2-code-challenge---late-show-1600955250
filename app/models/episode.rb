3e1d8b3c50aa820f8ff6a57255ae742067facbb8
gabrielhicks <gabrielhicks@mail.com>
2020-09-24 11:06:11 -0400

    Added seeds, and manually gave every episode an appearance

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,9 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def average_rating
+        total_ratings = self.appearances.map {|appearance| appearance.rating }.sum
+        average_ratings = total_ratings / self.appearances.count
+    end
 end
