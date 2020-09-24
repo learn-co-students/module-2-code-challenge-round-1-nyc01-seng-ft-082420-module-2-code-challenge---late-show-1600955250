b501fff07e83bb5bd29b6aa5cf8b1f7fad1f7ff3
jasonchilcott <jasonchilcottcode@gmail.com>
2020-09-24 11:37:17 -0400

    complete deliverables

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,5 @@ class Appearance < ApplicationRecord
   belongs_to :episode
 
   validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :guest, uniqueness: { scope: :episode }
 end
