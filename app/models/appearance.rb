98667d2b9c80ca9a4c442e3e448248902d8cc28e
Sean LaFlam <sean.laflam@gmail.com>
2020-09-24 11:35:58 -0400

    oh well

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -4,4 +4,9 @@ class Appearance < ApplicationRecord
 
    validates_inclusion_of :rating, in: 1..5
    #validates :episode_id && :guest_id, uniqueness: true
+    #if guest_id exists, in appearance, the episode_id must be unique
+
+   def guest_rating
+
+   end
 end
