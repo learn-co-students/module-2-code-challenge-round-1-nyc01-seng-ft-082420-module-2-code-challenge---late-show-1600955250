8a5d49fdfc39b13671efcc18f97e6fb9ed167964
ashabahmed <ashabahmed7@gmail.com>
2020-09-24 11:36:12 -0400

    final submission, all deliverables done except last due to no time.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,15 @@ class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
 
+    def episodes
+        self.appearances.map{|a| a.episode}
+    end
+
+    def sorting_episodes_by_rating
+
+    end
+
+# Couldn't finish last advanced guest show page in time, but I know i had to create helper methods in models
+# to connect the episodes and appearances and make methods to sort them by ratings and call that method or methods
+# in show page.
 end
