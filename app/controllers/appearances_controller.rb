3bff8b959514443562a44611708a7b98aded95b9
ashabahmed <ashabahmed7@gmail.com>
2020-09-24 10:30:47 -0400

    finished new appearance form and episode show page

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,18 @@
+class AppearancesController < ApplicationController
+  
+  
+  def new
+    @appearance = Appearance.new
+  end
+
+  def create
+    appearance = Appearance.create(appearance_params)
+    redirect_to episode_path(appearance.episode)
+  end
+
+  private
+
+  def appearance_params
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+  end
+end
