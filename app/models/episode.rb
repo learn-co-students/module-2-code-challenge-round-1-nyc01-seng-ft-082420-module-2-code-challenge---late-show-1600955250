bc23e93bc2fb71e9565d31237ceba5a3a7da7a9f
mahdihawary <Mahdihawary@gmail.com>
2020-09-24 11:29:01 -0400

    seocnd commit

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,17 @@
 class Episode < ApplicationRecord
     has_many :apearances
     has_many :guests, through: :apearances
+
+    def average_rating
+        average = 0
+        count = 0
+        self.apearances.each do |apearances|
+            count+=1
+            average = average + apearance.rating
+        end
+        average = average/count
+
+
+    end
+
 end
