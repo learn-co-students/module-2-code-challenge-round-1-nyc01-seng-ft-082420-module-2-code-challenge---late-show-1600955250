62be509d7671922ca61a9b8edd303e17d17fd37c
jasonchilcott <jasonchilcottcode@gmail.com>
2020-09-24 11:10:10 -0400

    built new appearance view

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -17,6 +17,6 @@ class AppearancesController < ApplicationController
     private
 
     def appearance_params
-        params.require(:appearance).permit!
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
     end
 end
