0c36b0c49714428905069396931f9ac23a0d5df6
D1yr <alexander.m.sahli@gmail.com>
2020-09-24 10:41:36 -0400

    appearance form working correctly, finishing create so i can post and make RESTful

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,5 +1,8 @@
 class AppearancesController < ApplicationController
     def new
+        @appearance = Appearance.new
+        @guest = Guest.all
+        @episode = Episode.all
     end
     #def create
     #end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guests, only: [:index]
   resources :episodes, only: [:index]
-  get "/appearances", to: "appearances#index", at: "appearances"
+  get "/appearances", to: "appearances#index", as: "appearances"
+  get "/appearances/new", to: "appearances#new", as: "new_appearances"
 end
