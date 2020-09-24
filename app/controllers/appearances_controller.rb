ef5c8a18d51c57845c04249ddcd9068f0f340d29
Sean LaFlam <sean.laflam@gmail.com>
2020-09-24 10:25:47 -0400

    Completed Appearance Form

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,23 @@
 class AppearancesController < ApplicationController
+  def new
+    @appearance = Appearance.new
+  end
+
+  def create
+    @appearance = Appearance.new(appearance_params)
+    if @appearance.valid?
+      @appearance.save
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def appearance_params
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+  end
+
+
+
 end
