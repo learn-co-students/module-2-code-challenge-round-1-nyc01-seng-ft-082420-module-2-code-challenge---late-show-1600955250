65092101ec718cb93d54b7bbe99bb70b134dc22c
Sean LaFlam <sean.laflam@gmail.com>
2020-09-24 11:32:12 -0400

    Got as far as I could

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,5 +2,6 @@ class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
 
-  validates :rating, numericality: {in: 1..5}
+   validates_inclusion_of :rating, in: 1..5
+   #validates :episode_id && :guest_id, uniqueness: true
 end
